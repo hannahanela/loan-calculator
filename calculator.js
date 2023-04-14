80787098a7c5ff78b68616fa7e63d3c20362806a
anela <anela89@gmail.com>
2023-04-13 20:54:10 -0700

    Refactored to reduce redundancy

--- calculator.js
+++ calculator.js
@@ -2,15 +2,18 @@
 // Find DOM element and hold onto it, so we don't have to search for it
 // every time we use it.
 const calcForm = document.getElementById("calc-form");
+const amountInput = document.getElementById("loan-amount");
+const yearsInput = document.getElementById("loan-years");
+const rateInput = document.getElementById("loan-rate");
+const paymentSpan = document.getElementById("calc-monthly-payment");
 
 
 /** Retrieve form values. Returns object: {amount, years, rate}. */
 
 function getFormValues() {
-  // TODO: handle form validation
-  let amount = document.getElementById("loan-amount").value;
-  let years = document.getElementById("loan-years").value;
-  let rate = document.getElementById("loan-rate").value;
+  let amount = amountInput.value;
+  let years = yearsInput.value;
+  let rate = rateInput.value;
 
   return { amount, years, rate };
 }
@@ -19,9 +22,14 @@ function getFormValues() {
 /** Calculate monthly payment and return. */
 
 function calcMonthlyPayment(amount, years, rate) {
-  let i = (rate / 100) / 12;
-  let n = years * 12;
-  let monthlyPayment = (amount * i) / (1 - (1 + i)**(-Math.abs(n)));
+  if (amount < 0) return 'Please enter a valid loan amount.'
+  if (rate <= 0) return 'Please enter a valid rate.'
+
+  let numPayments = (years % 2 === 0) ? years * 12 : false;
+  if (!numPayments) return 'Please enter a valid number of years.'
+
+  let interest = (rate / 100) / 12;
+  let monthlyPayment = (amount * interest) / (1 - (1 + interest)**(-Math.abs(numPayments)));
 
   return monthlyPayment;
 }
@@ -32,8 +40,6 @@ function calcMonthlyPayment(amount, years, rate) {
 function getFormValuesAndDisplayResults() {
   let { amount, years, rate } = getFormValues();
   let monthlyPayment = calcMonthlyPayment(amount, years, rate).toFixed(2);
-  let paymentSpan = document.getElementById("calc-monthly-payment");
-
   paymentSpan.innerText = monthlyPayment;
 }
 
@@ -41,13 +47,8 @@ function getFormValuesAndDisplayResults() {
 /** Set initial form values and show initial results. Called at app start. */
 
 function setInitialValues() {
-  let amountInput = document.getElementById("loan-amount");
   amountInput.value = 10000;
-
-  let yearsInput = document.getElementById("loan-years");
   yearsInput.value = 10;
-
-  let rateInput = document.getElementById("loan-rate");
   rateInput.value = 4.5;
 }
 
