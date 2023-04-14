0a51cb5f0c915fd16c8af75a1a9266ff1d24209e
anela <anela89@gmail.com>
2023-04-13 20:55:59 -0700

    Added more tests to check input validation

--- calculator.test.js
+++ calculator.test.js
@@ -1,3 +1,7 @@
 it('should calculate monthly rate', function () {
   expect(calcMonthlyPayment(10000, 10, 4.5)).toEqual(103.63840875701705);
+  expect(calcMonthlyPayment(-1, 10, 4.5)).toEqual('Please enter a valid loan amount.');
+  expect(calcMonthlyPayment(1000, 3.5, 4.5)).toEqual('Please enter a valid number of years.');
+  expect(calcMonthlyPayment(10000, 0, 4.5)).toEqual('Please enter a valid number of years.');
+  expect(calcMonthlyPayment(10000, 10, -1)).toEqual('Please enter a valid rate.');
 });
